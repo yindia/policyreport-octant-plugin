223d6d510f0f285ca1eda71392c62a156b5b6f87
Yuvraj <code@evalsocket.dev>
2021-06-27 21:21:33 +0530

    Fix cmd
    Signed-off-by: Yuvraj <code@evalsocket.dev>

---
+++ cmd/policyrepor-octant-plugin/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/evalsocket/policyreport-octant-plugin/pkg/plugin/settings"
+	"github.com/vmware-tanzu/octant/pkg/plugin/service"
+)
+
+var (
+	// Default wise GoReleaser sets three ldflags:
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+func main() {
+	log.SetPrefix("")
+
+	if err := run(os.Args); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+}
+
+func run(_ []string) (err error) {
+	name := settings.GetName()
+	description := settings.GetDescription(settings.VersionInfo{
+		Version: version,
+		Commit:  commit,
+		Date:    date,
+	})
+	capabilities := settings.GetCapabilities()
+	options := settings.GetOptions()
+	plugin, err := service.Register(name, description, capabilities, options...)
+	if err != nil {
+		err = fmt.Errorf("registering %s plugin: %w", name, err)
+		return
+	}
+	plugin.Serve()
+	return
+}
